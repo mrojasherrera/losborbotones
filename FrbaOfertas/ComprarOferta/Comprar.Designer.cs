d8b20e12e012435776db299162319c67dc9ddde8
miguel <mrojasherrer@gmail.com>
2019-11-30 16:39:52 -0300

    comprar Ofertas

---
+++ FrbaOfertas/ComprarOferta/Comprar.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace FrbaOfertas.ComprarOferta
+{
+    partial class Comprar
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.Cantidad = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.Oferta = new System.Windows.Forms.ComboBox();
+            this.Precio = new System.Windows.Forms.NumericUpDown();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.UsuarioTB = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.ComprarBtn = new System.Windows.Forms.Button();
+            this.VolverBtn = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Cantidad)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Precio)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.UsuarioTB);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(13, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(505, 73);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Cliente";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.Cantidad);
+            this.groupBox2.Controls.Add(this.label4);
+            this.groupBox2.Controls.Add(this.Oferta);
+            this.groupBox2.Controls.Add(this.Precio);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(13, 93);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(505, 187);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Comprar";
+            // 
+            // Cantidad
+            // 
+            this.Cantidad.Location = new System.Drawing.Point(321, 130);
+            this.Cantidad.Name = "Cantidad";
+            this.Cantidad.Size = new System.Drawing.Size(120, 20);
+            this.Cantidad.TabIndex = 14;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(74, 130);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 20);
+            this.label4.TabIndex = 13;
+            this.label4.Text = "CANTIDAD A COMPRAR:";
+            // 
+            // Oferta
+            // 
+            this.Oferta.FormattingEnabled = true;
+            this.Oferta.Location = new System.Drawing.Point(187, 43);
+            this.Oferta.Name = "Oferta";
+            this.Oferta.Size = new System.Drawing.Size(254, 21);
+            this.Oferta.TabIndex = 12;
+            // 
+            // Precio
+            // 
+            this.Precio.Location = new System.Drawing.Point(321, 88);
+            this.Precio.Name = "Precio";
+            this.Precio.Size = new System.Drawing.Size(120, 20);
+            this.Precio.TabIndex = 11;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(74, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(81, 20);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "PRECIO:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(74, 44);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(85, 20);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "OFERTA:";
+            // 
+            // UsuarioTB
+            // 
+            this.UsuarioTB.Enabled = false;
+            this.UsuarioTB.Location = new System.Drawing.Point(234, 26);
+            this.UsuarioTB.Name = "UsuarioTB";
+            this.UsuarioTB.Size = new System.Drawing.Size(151, 20);
+            this.UsuarioTB.TabIndex = 8;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(119, 27);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(96, 20);
+            this.label1.TabIndex = 7;
+            this.label1.Text = "USUARIO:";
+            // 
+            // ComprarBtn
+            // 
+            this.ComprarBtn.Location = new System.Drawing.Point(289, 308);
+            this.ComprarBtn.Name = "ComprarBtn";
+            this.ComprarBtn.Size = new System.Drawing.Size(109, 48);
+            this.ComprarBtn.TabIndex = 10;
+            this.ComprarBtn.Text = "Comprar";
+            this.ComprarBtn.UseVisualStyleBackColor = true;
+            // 
+            // VolverBtn
+            // 
+            this.VolverBtn.Location = new System.Drawing.Point(136, 308);
+            this.VolverBtn.Name = "VolverBtn";
+            this.VolverBtn.Size = new System.Drawing.Size(109, 48);
+            this.VolverBtn.TabIndex = 9;
+            this.VolverBtn.Text = "Volver";
+            this.VolverBtn.UseVisualStyleBackColor = true;
+            this.VolverBtn.Click += new System.EventHandler(this.VolverBtn_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::FrbaOfertas.Properties.Resources._185_1850575_las_compras_en_linea_venta_graphic_icono_online_png;
+            this.pictureBox1.Location = new System.Drawing.Point(536, 21);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(345, 259);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 11;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Comprar
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(907, 391);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.ComprarBtn);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.VolverBtn);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Comprar";
+            this.Text = "Form1";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.Cantidad)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Precio)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.NumericUpDown Cantidad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox Oferta;
+        private System.Windows.Forms.NumericUpDown Precio;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox UsuarioTB;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button ComprarBtn;
+        private System.Windows.Forms.Button VolverBtn;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
---
+++ FrbaOfertas/ComprarOferta/Comprar.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FrbaOfertas.ComprarOferta
+{
+    public partial class Comprar : Form
+    {
+        public Comprar(String unCliente)
+        {
+            InitializeComponent();
+            UsuarioTB.Text = unCliente;
+        }
+
+        private void VolverBtn_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
